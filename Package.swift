6af6f60a6525e899197c6ec9a9083094578de2c2
gsl-sebastienpecoul <sebastien.pecoul-ext@aviv-group.com>
2026-07-17 18:19:11 +0200

    Point GEOSwift dependency at immutable 11.2.0-b1

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "GEOSwiftMapKit", targets: ["GEOSwiftMapKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/gsl-sebastienpecoul/GEOSwift.git", exact: "11.2.0-binary")
+        .package(url: "https://github.com/gsl-sebastienpecoul/GEOSwift.git", exact: "11.2.0-b1")
     ],
     targets: [
         .target(
