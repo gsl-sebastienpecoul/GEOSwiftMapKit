5d61673beea79350d0f856a57d8420a659b17eb3
gsl-sebastienpecoul <sebastien.pecoul-ext@aviv-group.com>
2026-07-17 18:31:17 +0200

    Point GEOSwift dependency at 11.2.0-b2

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "GEOSwiftMapKit", targets: ["GEOSwiftMapKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/gsl-sebastienpecoul/GEOSwift.git", exact: "11.2.0-b1")
+        .package(url: "https://github.com/gsl-sebastienpecoul/GEOSwift.git", exact: "11.2.0-b2")
     ],
     targets: [
         .target(
